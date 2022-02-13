d4d617f91d26b77355866bce97220ca5640691a0
Abdelrahman Ibrahim <abdelramin30@gmail.com>
2022-02-14 00:54:53 +0200

    product from api

--- src/app/components/admin/adminproducts/adminproducts.component.ts
+++ src/app/components/admin/adminproducts/adminproducts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './../../../models/Product';
+import { ProductService } from './../../../services/product.service';
 
 @Component({
   selector: 'app-adminproducts',
@@ -6,10 +8,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminproducts.component.css']
 })
 export class AdminproductsComponent implements OnInit {
+  products:Product[]=[];
+  // Pagination parameters.
+  p: any = 1;
+  count: any = 3;
+  searchText:any;
+ constructor(private _productService:ProductService) { }
 
-  constructor() { }
+ ngOnInit(): void {
+   this._productService.get().subscribe(
+     (res: any) => {
+       console.log(JSON.stringify(res));
+       this.products = res.data;
+     }
+   );
+ }
+ add(product_name:string,description:string,image:any,image_path:string,subcat_id:number):void{
+   let product = new Product();
+   product.product_name=product_name;
+   product.description=description;
+   product.image=image;
+   product.image_path=image_path;
+   product.subcat_id=subcat_id;
+   this._productService.post(product).subscribe(
+     (response:any)=>{
+       this.products.push(product);
+     },
+     (error:any)=>{}
+   );
+ }
 
-  ngOnInit(): void {
-  }
+ delete(index:number):void
+ {
+   let product=this.products[index];
+   this._productService.delete(product.id)
+   .subscribe(
+     (response:any)=>{
+       const cf=confirm('Are U Sure Delete ?');
+       if(cf === true){
+         this.products.splice(index,1);
+       }else{
+         console.log('opps!');
+
+       }
+
+     },
+     (error:any)=>{}
+   );
+
+
+ }
+ edit(id:number){
+
+   this.products.forEach(
+     c=>{
+       if(c.id == id){
+         $("#uppid").prop('value',c?.id);
+         $("#productName").prop('value',c?.product_name);
+         $("#description").prop('value',c?.description);
+         $("#image").prop('value',c?.image);
+         $("#image_path").prop('value',c?.image_path);
+         $("#subcat_id").prop('value',c?.subcat_id);
+       }
+     }
+   );
+
+ }
+ product =new Product();
+ update(id:any,pName:any,pDesc:any,pImage:any,pImagePath:any,pSubcatId:any):void
+ {
+   this.product.product_name=pName;
+   this.product.description=pDesc;
+   this.product.image=pImage;
+   this.product.image_path=pImagePath;
+   this.product.subcat_id=pSubcatId;
+   this._productService.put(id,this.product)
+   .subscribe(
+     (response:any)=>{
+       window.location.reload();
+     },
+     (error:any)=>{}
+   );
+   //alert("Done");
+ }
 
 }
+
---
+++ src/app/models/Product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id: number = 0;
+  product_name: string = "";
+  description: string = "";
+  image:any='http://127.0.0.1:8000/public/products';
+  image_path:string='';
+  subcat_id: number = 0;
+
+}
