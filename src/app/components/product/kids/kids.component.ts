27c7415f6b0298a35a5a2d3c79dd641c65dff203
nada.h <nada.h22425@gmail.com>
2022-02-20 21:38:21 +0200

    all features in kids page

--- src/app/components/product/kids/kids.component.ts
+++ src/app/components/product/kids/kids.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryServiceService } from 'src/app/services/category-service.service';
 import { ProductService } from 'src/app/services/product.service';
 import { StoreService } from 'src/app/services/store.service';
+import { SubcategoryService } from 'src/app/services/subcategory.service';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-kids',
@@ -8,10 +11,16 @@ import { StoreService } from 'src/app/services/store.service';
   styleUrls: ['./kids.component.css']
 })
 export class KidsComponent implements OnInit {
+  public allsubcategories:any[]=[];
+  public subcategories:any[]=[];
   productsCategory:any[]=[];
   imagepath: any = 'http://127.0.0.1:8000/public/image/';
-  price:any[]=[];
-  constructor(private productService:ProductService,private storeService:StoreService) { }
+  price: any[] = [];
+  p: any = 1;
+  count: any = 9;
+  searchText:any;
+  constructor(private productService: ProductService, private storeService: StoreService, public header: HeaderComponent,
+    private _SubcategoryService:SubcategoryService,private _categoryService:CategoryServiceService) { }
 
   ngOnInit(): void {
     this.productService.getProductsCategory(3).subscribe(
@@ -23,6 +32,32 @@ export class KidsComponent implements OnInit {
     );
 
     this.getPrice();
+    this._categoryService.get().subscribe(
+      (res: any) => {
+        for(const i in res.data){
+          const id= res.data[i].id;
+          this._SubcategoryService.getSubCatForEachCategory(id).subscribe(
+            (res:any)=>{
+              this.subcategories=res.data;
+              console.log(this.subcategories);
+              
+                    this.allsubcategories.push(this.subcategories.map(m=>{return m}));
+
+                  // for(let i=0;i<res.data.length;i++){
+                  //   this.subcategories.push(res.data[i])
+                  //   this.subcategories=this.subcategories.map(m=>{return m});
+
+                  // }
+               },
+            (err:any)=>{
+              console.log(err);
+              
+            }
+          )
+        }
+        
+      }
+    ); 
   }
  
    
