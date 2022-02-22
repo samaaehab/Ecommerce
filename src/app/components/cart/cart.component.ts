a5cae6c87f6f21496bb12cb971bfbd50f58c3414
mohammedgalalfci <mohammedgalalfci@gmail.com>
2022-02-21 20:12:20 -0500

    add to cart and update

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { AppComponent } from 'src/app/app.component';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -12,9 +13,8 @@ export class CartComponent implements OnInit {
   products: any[] = [];
   productsInCart: any[] = [];
   cartCount:any;
-    
-    
-    constructor(private _userService: UserService, private router:Router) { }
+  totalPrice:number=0; 
+    constructor(private _userService: UserService, private router:Router,public myapp:AppComponent) { }
 
   ngOnInit(): void {
     for (var i = 0; i < localStorage.length; i++) {
@@ -28,10 +28,15 @@ export class CartComponent implements OnInit {
         this.cartCount=this.productsInCart.length;
 
       }
-      
     }
     
-    console.log(this.cartCount);
+    for (var i = 0; i < this.productsInCart.length; i++) {
+     
+     this.totalPrice+=Number(this.productsInCart[i][5]);
+     
+    }
+    
+    
   }
 
 
@@ -50,4 +55,25 @@ export class CartComponent implements OnInit {
     //   }
     // }
   }
+price:any;
+  getQty(qty:any){
+    return this.price=qty;
+    
+  }
+
+  updateCart(id:any,ProdName:any,Image:any,newPrice:any,qty:any,subTotal:any){
+    for (var i = 0; i < localStorage.length; i++) {
+      let a = localStorage.key(i);
+      if (a?.substring(0, 7)+id == 'product'+id) {
+       localStorage.removeItem(a?.substring(0, 7)+id);
+       localStorage.setItem('product' + id,ProdName + '#$' + Image + '#$' +newPrice + '#$' + id + '#$' + qty + '#$' + subTotal);      }
+      
+    }
+     
+    this.myapp.showInfo("Updated Successfuly","Updated");
+  }
+  getTotalPrice(){
+    //$("#totPrice").html((this.totalPrice).toString());
+  }
+
 }
