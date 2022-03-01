002984ae082eaa657bb3f1965a1f98f3b92770c2
samaaehab <samaaehab48@gmail.com>
2022-03-01 19:10:15 +0200

    edit veiw in subcategory page

--- src/app/components/product/subcategory/subcategory.component.ts
+++ src/app/components/product/subcategory/subcategory.component.ts
@@ -71,76 +71,76 @@ export class SubcategoryComponent implements OnInit {
     );
   }
 
-  getid(id: number) {
-    this.products.forEach(
-      c => {
-        if (c.id == id) {
-          var img = this.imagepath + c.image;
-          this.total = c.price - c.discount;
-          $("#exampleModalLabel1").html(c?.product_name);
-          $("#exampleModalLabel11").prop('value', c?.id);
-          $("#exampleModalLabel2").html(c?.description);
-          $("#exampleModalLabel4").html(String(this.total));
+  // getid(id: number) {
+  //   this.products.forEach(
+  //     c => {
+  //       if (c.id == id) {
+  //         var img = this.imagepath + c.image;
+  //         this.total = c.price - c.discount;
+  //         $("#exampleModalLabel1").html(c?.product_name);
+  //         $("#exampleModalLabel11").prop('value', c?.id);
+  //         $("#exampleModalLabel2").html(c?.description);
+  //         $("#exampleModalLabel4").html(String(this.total));
   
-          $("#exampleModalLabel3").prop('src', img);
-          // $("#exampleModalLabel3").prop('src')=c?.src;
-          // console.log(this.imagepath+c.image);
+  //         $("#exampleModalLabel3").prop('src', img);
+  //         // $("#exampleModalLabel3").prop('src')=c?.src;
+  //         // console.log(this.imagepath+c.image);
   
-          for (let i = 0; i < this.productStore.length; i++) {
-            if (id == this.productStore[i].product_id) {
-              // this.productStoreId.push(this.productStore[i].id);
-              // this.productSize.push(this.productStore[i].size);
-              // this.productColor.push(this.productStore[i].color);
-              this.allStore.push(this.productStore[i]);
+  //         for (let i = 0; i < this.productStore.length; i++) {
+  //           if (id == this.productStore[i].product_id) {
+  //             // this.productStoreId.push(this.productStore[i].id);
+  //             // this.productSize.push(this.productStore[i].size);
+  //             // this.productColor.push(this.productStore[i].color);
+  //             this.allStore.push(this.productStore[i]);
   
-            }
+  //           }
         
-          }
+  //         }
   
-          ;
+  //         ;
           
   
   
   
-        }
-      }
-    );
+  //       }
+  //     }
+  //   );
   
   
-  }
-  closeModel() {
+  // }
+  // closeModel() {
   
    
-    this.allStore = [];
-  }
-  addToCart(id: any, productSizeColor: any) {
-    this._activatedRoute.paramMap.subscribe(params => {
-      var subid = params.get('subid');
-      var catid = params.get('catid');
-      this._productService.getProductsForEachSubCategory(subid, catid).subscribe(
-        (res: any) => {
-          for (let p in res.data) {
-            this.products.push(res.data[p]);
+  //   this.allStore = [];
+  // }
+//   addToCart(id: any, productSizeColor: any) {
+//     this._activatedRoute.paramMap.subscribe(params => {
+//       var subid = params.get('subid');
+//       var catid = params.get('catid');
+//       this._productService.getProductsForEachSubCategory(subid, catid).subscribe(
+//         (res: any) => {
+//           for (let p in res.data) {
+//             this.products.push(res.data[p]);
          
-          }
+//           }
         
-          var product = this.products.find((p: any) => p.id == id);
-          if (localStorage.getItem('product' + productSizeColor) === null) {
-            let price = product.price - product.discount;
-            localStorage.setItem('product' + productSizeColor, product.id + "#$" + product.product_name + "#$" + this.imagepath + product.image + "#$" + 1 + "#$" + price + "#$" + productSizeColor + "#$" + price);
-            this.myapp.successmessage(product.product_name + " Added To Cart Successfuly");
-          }
-          else {
-            this.myapp.showWarning(product.product_name + " Already Added Before", "Oops");
+//           var product = this.products.find((p: any) => p.id == id);
+//           if (localStorage.getItem('product' + productSizeColor) === null) {
+//             let price = product.price - product.discount;
+//             localStorage.setItem('product' + productSizeColor, product.id + "#$" + product.product_name + "#$" + this.imagepath + product.image + "#$" + 1 + "#$" + price + "#$" + productSizeColor + "#$" + price);
+//             this.myapp.successmessage(product.product_name + " Added To Cart Successfuly");
+//           }
+//           else {
+//             this.myapp.showWarning(product.product_name + " Already Added Before", "Oops");
     
     
-          }
+//           }
     
-        }
+//         }
     
-      );
-    });
-}
+//       );
+//     });
+// }
 
 addToFav(id:any,ProdName:any,Image:any,newPrice:any){
   // let id = $("#id").prop('value');
