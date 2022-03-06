cc7122a3a5b39b545076dfe3cf37108cc64c3427
nada.h <nada.h22425@gmail.com>
2022-03-07 00:22:31 +0200

    imgs

--- src/app/components/product/home/home.component.ts
+++ src/app/components/product/home/home.component.ts
@@ -18,8 +18,8 @@ export class HomeComponent implements OnInit {
 public allsubcategories:any[]=[];
 public subcategories:any[]=[];
 men="assets/img/retro-man-dressed-shirt-lies-floor-posing.jpg";
-women="../../assets/img/Banner1.jpg";
-kids="../../assets/img/WhatsApp Image 2022-02-10 at 9.53.55 PM.jpeg";
+women="assets/img/Banner1.jpg";
+kids="assets/img/WhatsApp Image 2022-02-10 at 9.53.55 PM.jpeg";
 cat:any;
 subcat:any[]=[];
 productsCategory:any[]=[];
